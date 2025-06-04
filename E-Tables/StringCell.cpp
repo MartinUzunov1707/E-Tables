6ed615bb604557b764dc1517a1212cccf41e7b12
MartinUzunov1707 <muzun1707@gmail.com>
2025-06-04 19:11:03 +0300

    Add StringCell class.

--- E-Tables/StringCell.cpp
+++ E-Tables/StringCell.cpp
@@ -1,1 +1,18 @@
 #include "StringCell.h"
+
+StringCell::StringCell(char x, int y, MyString value)
+{
+	this->xValue = x;
+	this->yValue = y;
+	setValue(value);
+}
+
+void StringCell::setValue(MyString value)
+{
+	this->value = value;
+}
+
+double StringCell::evaluate()
+{
+	return 0.0;
+}
--- E-Tables/StringCell.h
+++ E-Tables/StringCell.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Cell.h"
 #include "MyString.h"
+
 class StringCell :
     public Cell
 {
+
 private:
    MyString value;
-
+public:
+    StringCell(char x, int y, MyString value);
+    void setValue(MyString value);
+    double evaluate() override;
 };
 
